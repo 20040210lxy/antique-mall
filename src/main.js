8c9ba412b2188fea4d552a26bc7139c78c98fdf7
Camellia <12492447+lxy10778x@user.noreply.gitee.com>
2023-12-28 22:30:11 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -1,7 +1,19 @@
 import Vue from 'vue'
+// 引入element-ui的全局样式
+import "element-ui/lib/theme-chalk/index.css"
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 引入element-ui插件
+import ElementUI from 'element-ui'
+// 引入axios插件
+import axios  from 'axios'
+import VueAxios from 'vue-axios'
+
+// 使用elemen-ui插件
+Vue.use(ElementUI);
+// 使用axios插件
+Vue.use(VueAxios,axios)
 
 Vue.config.productionTip = false
 
